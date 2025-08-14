82d7392222147c202b45b907a5bbcecc8ed153a3
uddiGitHub <u20cse1071@cit.ac.in>
2025-08-14 23:42:13 +0530

    update

--- models/Subscribe.jsx
+++ models/Subscribe.jsx
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const subscribeSchema = new mongoose.Schema({
+const SubscribeSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
